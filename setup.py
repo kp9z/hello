1a3a940391a751933900ea11fb76b65bc4ea1c21
Kenny <anhktrinh@gmail.com>
2023-04-21 11:39:30 +0700

    update filnd package

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='hello',
     version='0.1',
+    packages=find_packages(),
     description='A simple hello world package',
     url='https://github.com/kp9z/hello',
     author='Kenny',
